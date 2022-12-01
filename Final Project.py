29ea51e03f2d6569aa5d11461ec94e31a6d1b4cc
rgai9527 <rgai@uchicago.edu>
2022-12-01 15:31:56 -0600

    Text Process

--- Final Project.py
+++ Final Project.py
@@ -148,34 +148,31 @@ print(industries.loc['Advertising']['Beta'])
 ### Part 3
 
 # Set up the base path
-base_path = r'/Users/simonking/Documents/GitHub/Python II/homework-3-rgai9527-1'
-fig_path = os.path.join(base_path, 'question1_plot.png')  
+text_path = os.path.join(base_path, 'Texts') 
+textfig_path = os.path.join(base_path, 'Text_Processing_plot.png')  
 
 
 # Create a dictionary to restore all refugee text files
 reports = {}
-for file in os.listdir(base_path):
-    if file.endswith(".txt") and '2022' in file:
-        file_path = os.path.join(base_path, file)
+for file in os.listdir(text_path):
+    if file.endswith(".txt"):
+        file_path = os.path.join(text_path, file)
         with open(file_path, 'r') as f:
             reports[file[:-4]] = f.readlines()
 
-            
 
 # Convert the dictionary into dataframe, clean and merge the data, prepare for intepretation
 ## Conversion
 reports_clean = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in reports.items() ]))
 
 ## Drop blank rows
-reports_clean = reports_clean.drop([2,4,6,9,11])
+reports_clean = reports_clean.drop([2,4,6,8,10])
 
 ## Merge rows together,reset index, and clean the data
 reports_clean = reports_clean.apply(lambda x: ''.join(x.astype(str))).reset_index()
 reports_clean = reports_clean.rename({0: 'Text', 'index': 'Date'}, axis=1)
 reports_clean = reports_clean.applymap(lambda x: x.replace('\n', '. ').replace('..', '.'))
-reports_clean['Date'] = reports_clean['Date'].map(lambda x: dt.datetime.strptime(x,"%d%B%Y").strftime("%B/%d"))
 reports_clean = reports_clean.sort_values(by='Date').reset_index().drop(columns=['index'])
-
 
 
 # Analyze the sentiment of each article
@@ -188,17 +185,16 @@ for i in range(len(reports_clean['Text'])):
     reports_clean.iloc[i,2] = nlp(reports_clean.iloc[i,1])._.blob.polarity
 
 
-
 # Plot the sentiments
 fig = plt.figure()
 fig, ax = plt.subplots()
 plt.rcParams["figure.figsize"] = [8, 4]
 plt.rcParams["figure.autolayout"] = True
-ax.plot(reports_clean['Date'], reports_clean['Sentiment'], 'r*-')
+ax.plot(reports_clean['Date'], reports_clean['Sentiment'], 'b*-')
 ax.set_xlabel('Year 2022')
 ax.set_ylabel('Sentiment')
-ax.set_title('Q1 Plot')
-plt.savefig(fig_path)
+ax.set_title('US Stock Market Sentiments')
+plt.savefig(textfig_path)
 
 
 
